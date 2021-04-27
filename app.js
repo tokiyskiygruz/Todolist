06fee932295a7a441314b9ebd503bc2c5d1ebd8a
tokiyskiygruz <GreatRichardd@gmail.com>
2021-04-27 21:30:53 +0300

    fixed todolist

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-let input = document.querySelector('.todo-article_input')
-let btn = document.querySelector('.add-todo_button')
+let input = document.querySelector('.todo-article__input')
+let btn = document.querySelector('.add-todo__button')
 let todoUl = document.querySelector('.todo')
 
 
 btn.addEventListener('click', () => {
     let inputValue = input.value 
-    function AddTodo () {
+    function addTodo () {
 
-        let li = document.createElement('li');
-        li.innerHTML = inputValue
-        todoUl.append(li);
+        let todoTask = document.createElement('li');
+        todoTask.classList.add('task')
+        let taskText = document.createElement('span')
+        taskText.classList.add('task__text')
+        taskText.innerHTML = inputValue
+        todoTask.append(taskText)
+        todoUl.append(todoTask);
 
-        let check = document.createElement('i')
-        check.classList.add('fas')
-        check.classList.add('fa-check')
-        li.append(check)
+        let buttonWrapper = document.createElement('div')
+        todoTask.append(buttonWrapper)
+        let checkButton = document.createElement('button')
+        checkButton.classList.add('task__button')
+        buttonWrapper.append(checkButton)
+        let buttonImgCheck = document.createElement('i')
+        buttonImgCheck.classList.add('fas')
+        buttonImgCheck.classList.add('fa-check')
+        checkButton.append(buttonImgCheck)
 
-        let trash = document.createElement('i')
-        trash.classList.add('fas')
-        trash.classList.add('fa-trash-alt')
-        li.append(trash)
+        let trashButton = document.createElement('button')
+        buttonWrapper.append(trashButton)
+        trashButton.classList.add('task__button')
+        let buttonImgTrash = document.createElement('i')
+        buttonImgTrash.classList.add('fas')
+        buttonImgTrash.classList.add('fa-trash-alt')
+        trashButton.append(buttonImgTrash)
 
-        check.addEventListener('click', () => {
-            function completedToDo() {
-                li.classList.toggle('completed')
-            }
-            completedToDo();
+        checkButton.addEventListener('click', () => {
+            todoTask.classList.toggle('completed')
+            checkButton.classList.toggle('completed')
         })
 
-        trash.addEventListener('click', () => {
-            function deleteLi() {
-                li.remove();
-            }
-            deleteLi()
+        trashButton.addEventListener('click', () => {
+            todoTask.remove();
         })
         input.value = ''
     }
-    AddTodo();
+    addTodo();
 })
